934afeb0a08e920aa22926b3129e72eb2c32a923
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-09-24 23:11:55 +0300

    cache item pkg

--- item.go
+++ item.go
@@ -1,1 +1,12 @@
 package go_cache
+
+import "time"
+
+// Item represents a generic item that can be stored in a priority queue.
+// It contains a key of type Key, a value of type Value, an expiration time,
+// and an index for heap management.
+type Item[Key comparable, Value any] struct {
+	Key       Key       // Key is a unique identifier for the item.
+	Value     Value     // Value is the data associated with the item.
+	ExpiresAt time.Time // ExpiresAt is the time at which the item is considered expired.
+}
