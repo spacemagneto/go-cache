8d3914549014537944dd644c943d081248ae9398
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-09-25 00:02:31 +0300

    BenchmarkMemoryCacheSet

--- memory_cache_benchmark_test.go
+++ memory_cache_benchmark_test.go
@@ -1,1 +1,27 @@
 package go_cache
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func BenchmarkMemoryCacheSet(b *testing.B) {
+	// Create a new MemoryCache instance with a TTL of 1 minute.
+	cache := NewMemoryCache[string, int](context.Background(), 5*time.Minute, 1*time.Minute, 1000)
+
+	// Reset the benchmark timer to exclude setup time.
+	b.ReportAllocs()
+
+	b.ResetTimer()
+	// Run the benchmark to measure the performance of the Set method.
+	for i := 0; i < b.N; i++ {
+		// Set an item in the cache with a unique key and a TTL of 1 minute.
+		cache.Set(fmt.Sprintf("key%d", i), i, 0)
+	}
+
+	// Forcing the garbage collector (GC) to run to clear memory after benchmarking.
+	runtime.GC()
+}
