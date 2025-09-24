b5941486cbba3d47133f215dd270fd1dd02d2969
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-09-25 00:03:23 +0300

    BenchmarkMemoryCacheParallelSet

--- memory_cache_benchmark_test.go
+++ memory_cache_benchmark_test.go
@@ -3,6 +3,7 @@ package go_cache
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"runtime"
 	"testing"
 	"time"
@@ -21,6 +22,27 @@ func BenchmarkMemoryCacheSet(b *testing.B) {
 		// Set an item in the cache with a unique key and a TTL of 1 minute.
 		cache.Set(fmt.Sprintf("key%d", i), i, 0)
 	}
+
+	// Forcing the garbage collector (GC) to run to clear memory after benchmarking.
+	runtime.GC()
+}
+
+func BenchmarkMemoryCacheParallelSet(b *testing.B) {
+	// Create a new MemoryCache instance with a TTL of 1 minute.
+	cache := NewMemoryCache[string, int](context.Background(), 5*time.Minute, 1*time.Minute, 1000)
+
+	// Reset the benchmark timer to exclude setup time.
+	b.ReportAllocs()
+
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			i := rand.Int()
+			// Set an item in the cache with a unique key and a TTL of 1 minute.
+			cache.Set(fmt.Sprintf("key%d", rand.Int()), i, 0)
+		}
+	})
 
 	// Forcing the garbage collector (GC) to run to clear memory after benchmarking.
 	runtime.GC()
